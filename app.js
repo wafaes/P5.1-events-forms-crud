7b44d43396eb6d544bf125c40352bf300a1e09bf
wafaes <148322310+wafaes@users.noreply.github.com>
2024-12-13 03:55:03 +0100

    final
    😓😓😓😓😓😓😓😓😓😓😓😓😓😓

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-document.addEventListener("DOMContentLoaded", function(){
-    const form = document.getElementById("petForm");
-    const name = this.documentElement("name");
-    const description = this.documentElement("description");
-    const imageurl = this.documentElement("imageurl");//!!! alo mejor no está bien
-    const bday = this.documentElement("bday");
-    const price = this.documentElement("price");
-    const petcode = this.documentElement("petcode");
-    const sold = this.documentElement("sold");
+import {listOfPets} from "./listOfPets.js";
 
+document.addEventListener("DOMContentLoaded", function(){
+  const name = document.getElementById("name");
+  const description = document.getElementById("description");
+  const imageurl = document.getElementById("imageurl");
+  const bday = document.getElementById("bday");
+  const price = document.getElementById("price");
+  const code = document.getElementById("code");
+  const sold = document.getElementById("sold");
+ 
+
+    const editContainer = document.getElementById("editContainer");
+    const editForm = document.getElementById("editForm");
+    const editName = document.getElementById("editName");
+    const editDescription = document.getElementById("editDescription");
+    const editImg = document.getElementById("editImg");
+    const editBday = document.getElementById("editBday");
+    const editPrice = document.getElementById("editPrice");
+    const editSold = document.getElementById("editSold");
+    const editCode = document.getElementById("editCode");  
 
     /*function addPet(namee, description, imageurl, birthdate, price, petcode, sold); {
         const newPet = {
@@ -22,161 +33,262 @@ document.addEventListener("DOMContentLoaded", function(){
         this.pets.push(newPet);
     }*/
 
+        /* copiao */
+        const seePetsButton = document.getElementById("see-pets");
+        const petListContainer = document.querySelector(
+          ".row.justify-content-center.align-items-center.pet-list"
+        );
 
 
     function validateForm(){
         let isValid = true;
-        
 
-        if (!isValidName(namee.value)) {
-            markFieldAsNotValid(namee);
-            isValid = false;
-          } else {
-            markFieldAsValid(namee);
-          }
-        
-
-        if (!isValidPassword(description.value)) {
-            markFieldAsNotValid(description);
-            isValid = false;
-          } else {
-            markFieldAsValid(description);
-          }
-
-
-
-        if (!isValidImage(image.value)) {
-            markFieldAsNotValid(image);
-            isValid = false;
-          } else {
-            markFieldAsValid(image);
-          }
-
-
-        
-        if (!isValidName(bday.value)) {
-            markFieldAsNotValid(bday);
-            isValid = false;
-          } else {
-            markFieldAsValid(bday);
-          }
-
-
-        if (!isValidName(price.value)) {
-            markFieldAsNotValid(price);
-            isValid = false;
-          } else {
-            markFieldAsValid(price);
-          }
-
-
-        if (!isValidName(petcode.value)) {
-            markFieldAsNotValid(petcode);
-            isValid = false;
-          } else {
-            markFieldAsValid(petcode);
-          }
-
-
-        if (!isValidName(sold.value)) {
-            markFieldAsNotValid(sold);
-            isValid = false;
-          } else {
-            markFieldAsValid(sold);
-          }
-
-
-        return isValid;
-
-    }
-
-
-
-    function isValidName(){ //must be completed 
-        let textInputs = document.querySelectorAll('input[type="text"]');
-            if (input.value.trim() === "") {  // Check if the input is empty
-                markFieldAsNotValid(nameInput, "Dog's name is rquired");
-                return false;  // Return false to prevent form submission      
-    }
-    return true;  // If all fields are filled, return true
-    }
-
-    function isValidDescription(){
-        let descriptionInput = document.querySelector('input[name="description"]');
-        if(descriptionInput.value.trim()== ""){
-            markFieldAsNotValid(descriptionInput, "Description is required");
-            return false;
+        if (!checkEmptyInputs(name)) {
+          markFieldAsNotValid(name, "Name cannot be empty");
+          isValid = false;
+        } else {
+          markFieldAsValid(name);
         }
-        return true;
-    }
-
-    function isValidImage(){//must be completed ; valid ; .jpg .jpeg .png .gif
-        let imageInput = document.querySelector('input[name="imageurl"]');
-        let imageUrl = imageInput.value.trim();
-        let validImageExtensions = /\.(jpg|jpeg|png|gif)$/i;
-        if (!validImageExtensions.test(imageUrl)) {
-            markFieldAsNotValid(imageInput, "Image URL must be a valid image file (.jpg, .jpeg, .png, .gif).");
-            return false;
-        }
-        return true;
-    }
-
-    function isValidDate(){//must be completed ; not future
-        let dateInput = document.querySelector('input[name="bday"]');
-        let birthdate = new Date(dateInput.value);
-        let today = new Date();
-        if (birthdate > today) {
-            markFieldAsNotValid(dateInput, "This pet hasn't born yet 😨.");
-            return false;
-        }
-        return true;
-    }
-
-    function isValidPrice(){//must be completed  ; grater than 0
-        let priceInput = document.querySelector('input[name="price"]');
-        let price = parseFloat(priceInput.value);
-        if (isNaN(price) || price <= 0) {
-            markFieldAsNotValid(priceInput, "Is this pet free?? Correct the prize!!.");
-            return false;
-        }
-        return true;
-    }
-
-    function isValidPetCode(){//must be completed ; ABC123 format
-        let petCodeInput = document.querySelector('input[name="petcode"]');
-        let petCode = petCodeInput.value.trim();
-        let petCodePattern = /^[A-Z]{3}\d{3}$/; // ABC123 format
-        if (!petCodePattern.test(petCode)) {
-            markFieldAsNotValid(petCodeInput, "Pet code must be in the format ABC123.");
-            return false;
-        }
-        return true;
-    }
-
-    function isSold(){
-        const acceptCheckbox = document.querySelector('input[name="accept"]');
-        // No validation required for the checkbox; it's optional.
-        return true;
-    }
-
-
-    function markFieldAsValid(element) {
-        element.parentNode.classList.remove("is-not-valid-field");
-      }
-
-
-    function markFieldAsNotValid(element, message="Invalid field. Insert a valid value"){
-        let errorMessageElement = element.parentNode.querySelector(".error-message");
-        if (errorMessageElement) {
-            errorMessageElement.textContent = message;
-        }
-        element.classList.add("is-not-valid-field");  // Add a class to style invalid fields (you can define this class in CSS)
-        /*if(message){
-            element.parseNode.querySelector(".error-message").textContent = message;
-        }
-            element.parseNode.classList.add("is-not-valid-field");*/
-    }
     
-   
+        if (!checkEmptyInputs(description)) {
+          markFieldAsNotValid(description, "Description cannot be empty");
+          isValid = false;
+        } else {
+          markFieldAsValid(description);
+        }
+    
+        let urlIsValid = true;
+    
+        if (!checkEmptyInputs(imageurl)) {
+          markFieldAsNotValid(imageurl, "URL cannot be empty");
+          isValid = false;
+          imageurl = false;
+        } else {
+          markFieldAsValid(imageurl);
+        }
+    
+        if (imageurl && !checkImageUrl(imageurl.value)) {
+          markFieldAsNotValid(
+            imageurl,
+            "Image URL must be a valid .jpg, .jpeg, .png, or .gif file"
+          );
+          isValid = false;
+        } else {
+          markFieldAsValid(imageurl);
+        }
+    
+        let birthdayIsValid = true;
+    
+        if (!checkEmptyInputs(bday)) {
+          markFieldAsNotValid(bday, "Pet birthday cannot be empty");
+          isValid = false;
+          bdayIsValid = false;
+        } else {
+          markFieldAsValid(bday);
+        }
+    
+        if (bdayIsValid && !checkFuture(bday.value)) {
+          markFieldAsNotValid(bday, "Your pet cannot be from the future");
+          isValid = false;
+        } else {
+          markFieldAsValid(bday);
+        }
+    
+        if (isNaN(price.value) || price.value <= 0) {
+          markFieldAsNotValid(price, "Pet price must be a positive number");
+          isValid = false;
+        } else {
+          markFieldAsValid(price);
+        }
+    
+        if (!checkEmptyInputs(code)) {
+          markFieldAsNotValid(code, "Petcode input cannot be empty");
+          isValid = false;
+        } else {
+          markFieldAsValid(code);
+        }
+    
+        if (!checkCodePet(code)) {
+          markFieldAsNotValid(
+            code,
+            "Petcode needs to be formed by three leters and three numbers"
+          );
+          isValid = false;
+        } else {
+          markFieldAsValid(code);
+        }
+        return isValid;
+    }
 
+
+function checkEmptyInputs(input) {
+  return input.value.trim().length > 0;
+}
+
+
+function checkImageUrl(url) {
+  const imagePattern = /\.(jpg|jpeg|png|gif)$/i;
+  return imagePattern.test(trimmedUrl);
+}
+
+
+
+function checkFuture(birthdate) {
+  const bir = new Date(birthdate);
+  const today = new Date();
+  if (bir.getFullYear() > today.getFullYear()) {
+    return false;
+  } else if (bir.getFullYear() === today.getFullYear()) {
+    if (bir.getMonth() > today.getMonth()) {
+      return false;
+    } else if (bir.getMonth() === today.getMonth()) {
+      if (bir.getDate() > today.getDate()) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+
+
+function checkCodePet(code) {
+  const codePattern = /^[A-Za-z]{3}\d{3}$/; //Check notes because I won't remember how to do this in 2 days
+  return codePattern.test(code.value);
+}
+
+function markFieldAsNotValid(element, message) {
+  const errorMessage = element.parentNode.querySelector(".error-message");
+  errorMessage.textContent = message;
+  errorMessage.style.display = "block";
+  element.classList.add("is-not-valid-field");
+}
+
+function markFieldAsValid(element) {
+  const errorMessage = element.parentNode.querySelector(".error-message");
+  errorMessage.style.display = "none";
+  element.classList.remove("is-not-valid-field");
+}
+
+
+function renderPets(petList) {
+  const div = document.querySelector(
+    ".row.justify-content-center.align-items-center.pet-list"
+  ); 
+  div.innerHTML = "";
+  petList.forEach((pet) => {
+    const petItemContainer = document.createElement("div");
+    petItemContainer.classList.add("col-12", "col-md-6", "col-lg-3", "mb-4");
+
+    const petItem = document.createElement("div");
+    petItem.classList.add("card", "mt-2", "h-100");
+    petItem.innerHTML = `<img class="card-img-top" src="${
+      pet.imageUrl
+    }" alt="Title" />
+      <div class="card-body">
+          <h4 class="card-title">Name: ${pet.name}</h4>
+          <p class="card-text" style="font-weight: bold;">Description: <span> ${
+            pet.description
+          } </span></p>
+          <p class="card-text" style="font-weight: bold;">Price: <span> ${
+            pet.price
+          } €  </span></p>
+          <p class="card-text" style="font-weight: bold;">Birthday: <span> ${
+            pet.birthday
+          } </span></p>
+          <p class="card-text" style="font-weight: bold;">Code: <span> ${
+            pet.code
+          } </span> </p>
+          <p class="card-text" style="font-weight: bold;">Does this pet have an owner? : <span> ${
+            pet.sold
+              ? "Yes! It has an owner"
+              : "Now they got an owner!"
+          }</span></p>
+          <button class="btn btn-warning mt-2" data-pet-code="${
+            
+            pet.code
+          }">Delete Pet</button>
+          <button class="btn btn-primary mt-2" data-code="${
+            pet.code
+          }">Edit Pet</button>
+      </div>
+    `;
+    petItemContainer.appendChild(petItem);
+    div.appendChild(petItemContainer);
+  });
+
+
+  const editButtons = div.querySelectorAll("button.btn-info");
+  editButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      const code = event.target.getAttribute("data-code");
+      const petToEdit = petListInstance
+        .readPets()
+        .find((pet) => pet.code === code);
+
+      editName.value = petToEdit.name;
+      editDescription.value = petToEdit.description;
+      editImg.value = petToEdit.imageUrl;
+      editBday.value = petToEdit.birthday;
+      editPrice.value = petToEdit.price;
+      editSold.checked = petToEdit.sold;
+      editCode.value = petToEdit.code;
+
+      editContainer.style.display = "block";
+    });
+  });
+}
+
+  editForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const updatedPet = {
+    name: editName.value,
+    description: editDescription.value,
+    imageurl: editImg.value,
+    bday: editBirth.value,
+    price: parseFloat(editPrice.value),
+    code: editCode.value,
+    sold: editSold.checked,
+  };
+  petListInstance.editPet(updatedPet);
+  renderPets(petListInstance.readPets());
+});
+
+
+seePetsButton.addEventListener("click", (event) => {
+  petListContainer.style.display = "inline-flex"; //This over here took me like 2 hours MAAAAAAAAAAAAAAAAAAAAAAAN
+  renderPets(petListInstance.readPets());
+});
+
+function deletePet(code) {
+  console.log("Deleting pet with code:", code);
+  petListInstance.deletePet(code);
+  
+}
+
+petForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  if (validateForm()) {
+    const newPet = {
+      name: name.value,
+      description: description.value,
+      imageurl: imageurl.value,
+      bday: bday.value,
+      price: parseFloat(price.value),
+      code: code.value,
+      sold: sold.checked,
+    }; 
+
+    petListInstance.addPet(newPet);
+    console.log("Pet added succesfully");
+    
+    console.log(petListInstance);
+  } else {
+    console.log("Form is not valid.");
+  }
+});
+  //xxxzxcdhfjdnjdnkcxkc
+   
 });
